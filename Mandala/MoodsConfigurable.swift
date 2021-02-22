27b4634a842209840398565983615869edb5f75e
kranthi pyreddy <kra330@gmail.com>
2021-02-21 23:41:36 -0500

    Lab 3: Part A: Extensions, Controls and Animations

---
+++ Mandala/MoodsConfigurable.swift
@@ -0,0 +1,8 @@
+//  MoodsConfigurable.swift
+//  Mandala
+//  Created by Kranthi Pyreddy on 2/21/21.
+import Foundation
+//Implementing a new protocol
+    protocol MoodsConfigurable {
+        func add(_ moodEntry: MoodEntry)
+    }
--- Mandala/UIColor+Mandala.swift
+++ Mandala/UIColor+Mandala.swift
@@ -5,12 +5,12 @@ import UIKit
 
 extension UIColor {
     //Adding static properties for new colors 
-        static let angry = UIColor(named: "angryRed")!
-        static let confused = UIColor(named: "confusedPurple")!
-        static let crying = UIColor(named: "cryingLightBlue")!
-        static let goofy = UIColor(named: "goofyOrange")!
-        static let happy = UIColor(named: "happyTurquoise")!
-        static let meh = UIColor(named: "mehGray")!
-        static let sad = UIColor(named: "sadBlue")!
-        static let sleepy = UIColor(named: "sleepyLightRed")!
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
 }
